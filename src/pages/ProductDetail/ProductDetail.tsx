e7cc8f6084a81d5855ea9f0d3b0a7a208bcf154b
franklinlugo <franklinlugo2805@gmail.com>
2021-02-11 22:15:04 -0300

    product detail layout

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -12,7 +12,28 @@ const ProductDetail: React.FC = () => {
   return (
     <StyledProductDetail>
       <Breadcrumbs />
-      <span>id: {id}</span>
+      <div className="productCard">
+        <div className="productImage">
+          <div className="image">
+            <img src="https://via.placeholder.com/400" alt="img" />
+          </div>
+          <div className="info">
+            <p className="status">Nuevo - 294 vendidos</p>
+            <h3 className="title">Deco Reverse Sombrero Oxford</h3>
+            <span className="price">$ 1980</span>
+            <button className="buyBtn" type="button">
+              Comprar
+            </button>
+          </div>
+        </div>
+        <div className="productDescription">
+          <h5 className="title">dd</h5>
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur neque voluptates accusamus quisquam fugit
+            possimus, itaque consequatur animi modi similique?
+          </p>
+        </div>
+      </div>
     </StyledProductDetail>
   );
 };
--- src/pages/ProductDetail/ProductDetailStyles.ts
+++ src/pages/ProductDetail/ProductDetailStyles.ts
@@ -2,4 +2,65 @@ import styled from 'styled-components';
 
 export const StyledProductDetail = styled.div`
   padding: 0 6rem;
+  .productCard {
+    background-color: ${({ theme }) => theme.palette.white};
+    padding: 2rem;
+    > .productImage {
+      display: flex;
+      gap: 2rem;
+      > .image {
+        flex: 2;
+        display: flex;
+        justify-content: center;
+        img {
+          /* width: 100%;
+          height: 300px; */
+          max-width: 100%;
+        }
+      }
+      > .info {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        > .status {
+          font-size: 0.875rem;
+          line-height: 1;
+          margin-bottom: 1rem;
+        }
+        > .title {
+          font-size: 1.5rem;
+          line-height: 1;
+          margin-bottom: 2rem;
+        }
+        > .price {
+          font-size: 2.875rem;
+          line-height: 1;
+          margin-bottom: 2rem;
+        }
+        > .buyBtn {
+          padding: 1rem 0;
+          font-size: 1rem;
+          line-height: 1;
+          border-radius: 8px;
+          cursor: pointer;
+          color: ${({ theme }) => theme.palette.white};
+          background-color: ${({ theme }) => theme.palette.dodgerBlue};
+        }
+      }
+    }
+    > .productDescription {
+      width: 66%;
+      margin-top: 6rem;
+      > .title {
+        font-size: 1.75rem;
+        line-height: 1;
+        margin-bottom: 2rem;
+      }
+      > .paragraph {
+        font-size: 1rem;
+        line-height: 1.2;
+        color: ${({ theme }) => theme.palette.dustyGray};
+      }
+    }
+  }
 `;

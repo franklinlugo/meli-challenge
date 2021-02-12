cd452e95431c0cd1b04828dab2c243cc2a5939da
franklinlugo <franklinlugo2805@gmail.com>
2021-02-12 02:34:46 -0300

    feat: api get item details

--- server/src/routes/items.js
+++ server/src/routes/items.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 
 const router = express.Router();
 
-function buildItems(response) {
+function buildItemsList(response) {
   const { results } = response;
   return results.map((item) => ({
     id: item.id,
@@ -32,6 +32,23 @@ function buildCategories(response) {
   }
 }
 
+function buildItem(item, description) {
+  return {
+    id: item.id,
+    title: item.title,
+    price: {
+      currency: item.currency_id,
+      amount: item.price,
+      decimals: 0,
+    },
+    picture: item.pictures[0].url,
+    condition: item.condition,
+    free_shipping: item.shipping.free_shipping,
+    sold_quantity: item.sold_quantity,
+    description: description.plain_text
+  };
+}
+
 router.get('/items', async (req, res) => {
   const searchTerm = req.query.search;
   try {
@@ -39,7 +56,7 @@ router.get('/items', async (req, res) => {
     const response = await rawResponse.json();
 
     const author = { name: 'Franklin', lastName: 'Lugo' };
-    const items = buildItems(response);
+    const items = buildItemsList(response);
     const categories = buildCategories(response);
 
     res.status(200).json({ author, categories, items });
@@ -47,5 +64,23 @@ router.get('/items', async (req, res) => {
     res.status(400).send();
   }
 });
+
+router.get('/items/:id', async (req, res) => {
+  const id = req.params.id;
+  const  urls = [ `https://api.mercadolibre.com/items/${id}`, `https://api.mercadolibre.com/items/${id}/description` ];
+  const requests = urls.map(url => fetch(url));
+
+  try {
+    const [itemRawResponse, descriptionRawResponse] = await Promise.all(requests);
+    const [item, description] = await Promise.all([itemRawResponse.json(), descriptionRawResponse.json()]);
+
+    const author = { name: 'Franklin', lastName: 'Lugo' };
+    const itemObj = buildItem(item, description);
+
+    res.status(200).json({author, item: itemObj});
+  } catch (error) {
+    res.status(400).send();
+  }
+})
 
 export default router;
